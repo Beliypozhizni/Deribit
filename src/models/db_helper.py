36fc4299fe6526063dc7261eb9d50d6a9035de7b
Beliy <beliypozhizni@gmail.com>
2026-01-31 15:39:34 +0300

    Session closure in the context manager has been removed

--- src/models/db_helper.py
+++ src/models/db_helper.py
@@ -21,7 +21,6 @@ class DatabaseHelper:
     async def session_dependency(self) -> AsyncGenerator[Any, Any]:
         async with self.session_factory() as session:
             yield session
-            await session.close()
 
 
 db_helper = DatabaseHelper(url=settings.db_url, echo=settings.db_echo)
