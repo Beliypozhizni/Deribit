a7a24a0999399116e8975b4998726c5a65f8a00b
Beliy <beliypozhizni@gmail.com>
2026-01-31 15:42:45 +0300

    Added saving prices

--- src/worker/tasks.py
+++ src/worker/tasks.py
@@ -10,24 +10,33 @@ from src.deribit.client import (
     DeribitRateLimited,
     DeribitUnavailable,
 )
+from src.prices.crud import create_prices
+from ..models.db_helper import db_helper
+from ..prices.schemas import PriceBase
 
 logger = get_task_logger(__name__)
+
+event_loop = asyncio.new_event_loop()
+asyncio.set_event_loop(event_loop)
 
 TICKERS = ("btc_usd", "eth_usd")
 
 
-async def _collect_prices_async() -> list[dict]:
+async def _collect_prices_async() -> list[PriceBase]:
     async with DeribitClient() as client:
-        dtos = await client.get_index_prices(TICKERS)
+        prices = await client.get_index_prices(TICKERS)
 
-    return [
-        {
-            "ticker": dto.ticker,
-            "price": dto.price,
-            "captured_ts_ms": dto.captured_ts_ms,
-        }
-        for dto in dtos
-    ]
+    return prices
+
+
+async def _save_prices(prices: list[PriceBase]) -> None:
+    async with db_helper.session_factory() as session:
+        await create_prices(session=session, prices_in=prices)
+
+
+async def _collect_and_save_prices_async():
+    prices = await _collect_prices_async()
+    await _save_prices(prices)
 
 
 def _utc_now_iso() -> str:
@@ -42,7 +51,7 @@ from .celery_app import celery_app
 
 
 @celery_app.task(
-    name="src.worker.tasks.collect_prices",
+    name="src.worker.tasks.collect_and_save_prices",
     bind=True,
     autoretry_for=(DeribitUnavailable, DeribitRateLimited),
     retry_backoff=True,
@@ -50,23 +59,15 @@ from .celery_app import celery_app
     retry_jitter=True,
     retry_kwargs={"max_retries": 5},
 )
-def collect_prices(self) -> list[dict]:
-    logger.info("Collecting prices at %s", _utc_now_iso())
+def collect_and_save_prices(self):
+    logger.info("Collecting and saving prices at %s", _utc_now_iso())
 
     try:
-        result = asyncio.run(_collect_prices_async())
+        event_loop.run_until_complete(_collect_and_save_prices_async())
     except Exception as exc:
         if _is_transient_exc(exc):
             raise
-        logger.exception("Non-retryable error while collecting prices: %r", exc)
-        raise
-
-    for item in result:
-        logger.info(
-            "PRICE %s = %s (captured_ts_ms=%s)",
-            item["ticker"],
-            item["price"],
-            item["captured_ts_ms"],
+        logger.exception(
+            "Non-retryable error while collecting and saving prices: %r", exc
         )
-
-    return result
+        raise
