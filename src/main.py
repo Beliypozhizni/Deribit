f2628cbba62861fe2a64b9769a602a33a600be21
Beliy <beliypozhizni@gmail.com>
2026-01-31 18:58:00 +0300

    Added router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from src.api_v1 import router as router_v1
+from src.config import settings
 
 app = FastAPI()
+
+app.include_router(router_v1, prefix=settings.api_v1_prefix)
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,4 +17,4 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("src.app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
